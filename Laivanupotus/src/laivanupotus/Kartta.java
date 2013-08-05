84c99be6301f5754e995e4f39143eb3617c44dc9
Tekdino <tekdino@gmail.com>
2013-08-05 18:02:26 +0300

    Kartta-luokan alku

---
+++ Laivanupotus/src/laivanupotus/Kartta.java
@@ -0,0 +1,13 @@
+package laivanupotus;
+
+/**
+ *Sisältää kaksi taulukollista ruutuja joista toinen näytetään omana ja toinen 
+ *vastustajana (laivat näkyvät vain kun niihin on osuttu)
+ */
+public class Kartta {
+    
+    public Kartta() {
+        Ruutu[][] omaPuoli = new Ruutu[10][10];
+        Ruutu[][] vihuPuoli = new Ruutu[10][10];
+    }
+}
