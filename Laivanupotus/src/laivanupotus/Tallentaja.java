f2795ad26315d94edd467b0d9dc218d79e662ddb
Tekdino <tekdino@gmail.com>
2013-08-21 23:44:02 +0300

    Tallentajan runko

--- Laivanupotus/src/laivanupotus/Tallentaja.java
+++ Laivanupotus/src/laivanupotus/Tallentaja.java
@@ -5,4 +5,23 @@ package laivanupotus;
  */
 public class Tallentaja {
     
+    public Tallentaja() {
+        // TODO kentät jotka sisältää listan
+    }
+    
+    public void lataaTiedosto() {
+        // TODO tuo top listan kenttiin
+    }
+    
+    public void lisaaListaan() {
+        // TODO lisää listaan tiedon
+    }
+    
+    public void jarjestaLista() {
+        // TODO järjestää listan
+    }
+    
+    public void tallennaTiedosto() {
+        // TODO tallentaa kentät tiedostoon
+    }
 }
