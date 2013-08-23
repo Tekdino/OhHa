a5f9df2ff9332a93284d3876c85fa198f4c324f3
Tekdino <tekdino@gmail.com>
2013-08-23 18:09:48 +0300

    Paaluokan testausta kasin

--- Laivanupotus/src/laivanupotus/Laivanupotus.java
+++ Laivanupotus/src/laivanupotus/Laivanupotus.java
@@ -1,19 +1,20 @@
-/*
- * Laivanupotuspeli
- */
 package laivanupotus;
 
-/**
- *
- */
+
 public class Laivanupotus {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         //pikatesti
         Kartta kartta = new Kartta(5,5);
+        Ampuja ampuja = new Ampuja(kartta);
+        Laskuri laskuri = new Laskuri(kartta);
+        Laivaaja laivaaja = new Laivaaja(laskuri);
+        laivaaja.laivaa(3, 0, 1, 2, true);
+        
+        for (int i=0; i<10; i++) {
+            ampuja.isku();
+        }
         kartta.visualisoi();
     }
 }
