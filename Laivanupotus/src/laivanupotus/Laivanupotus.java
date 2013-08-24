e503da41c57d3ab34c873e25f7c3755d66d377c4
Tekdino <tekdino@gmail.com>
2013-08-24 20:47:59 +0300

    Pelimekaniikan alku

--- Laivanupotus/src/laivanupotus/Laivanupotus.java
+++ Laivanupotus/src/laivanupotus/Laivanupotus.java
@@ -11,6 +11,30 @@ public class Laivanupotus {
         int korkeus = kysyLuku("Kartan korkeus ruutuina?");
         
         Kartta kartta = new Kartta(leveys, korkeus);
+        Ampuja ampuja = new Ampuja(kartta);
+        Laskuri laskuri = new Laskuri(kartta);
+        Laivaaja laivaaja = new Laivaaja(laskuri);
+        
+        //Lisätään laivat
+        int maara = kysyLuku("Montako laivaa lisätään?");
+        
+        for (int i=0; i<maara; i++) {
+        int tyyppi = kysyLuku("Laivan " + i+1 + "/" + maara + " pituus?");
+        int asento = kysyLuku("Laivan asento? 1=vaaka, 2=pysty");
+        int sarake = kysyLuku("Laivan vasemmanpuoleisimman/ylimmän ruudun sarakekoordinaatti?");
+        int rivi = kysyLuku("Laivan vasemmanpuoleisimman/ylimmän ruudun rivikoordinaatti?");
+        laivaaja.laivaa(tyyppi, rivi, sarake, asento, true);
+        
+        //Arpoo samalla vastustajalle saman kokoisen laivan
+        boolean onnistuu;
+        do {
+        laivaaja.arvoRuutu();
+        onnistuu = laivaaja.tunnusteleLaiva(tyyppi, laivaaja.vapaaY, laivaaja.vapaaX);
+        } while (onnistuu == false);
+        kartta.visualisoi();
+        }
+        
+        //Laivat lisätty, peli alkaa
     }
     
     /**
